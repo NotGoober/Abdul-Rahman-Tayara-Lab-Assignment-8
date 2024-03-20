b661bc3cf39b0622d90b81db51e24e888996c9a8
NotGoober <89048507+NotGoober@users.noreply.github.com>
2024-03-20 12:03:10 -0400

    Update main.py
    Upgraded the script to take multiple inputs

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #python script that determines if you entered and even or odd number
 
 def main():
+  def determine_num_of_nums():
+    num_of_nums = int(input('Amount of numbers to determine: '))
+    return num_of_nums
 
-  def check_num():
-    input_num = int(input('Number to be determined: '))
-    if input_num % 2 == 0:
-      return 'Even'
-    else:
-      return 'Odd'
-  
     
-  print(check_num())
+  def gather_nums():
+      nums_list = []
+      for x in range(determine_num_of_nums()):
+        nums_list.append(int(input('Number to be determined: ')))
+      return nums_list
+    
+  def calculat_and_print():
+      for num in gather_nums():
+        if num % 2 == 0:
+          print('Even')
+        else:
+          print('Odd')
+          
+  calculat_and_print()
 
 main()
